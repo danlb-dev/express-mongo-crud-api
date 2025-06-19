c757da6c173b6d5d2145e2b44cbaf5f1d986b726
Daniel LB <danlb48986@gmail.com>
2025-06-19 14:30:34 -0600

    connecting app to database

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import swaggerUi from 'swagger-ui-express';
 import yaml from 'yamljs';
 
-// Routes
 import clientsRouter from './routes/client.route';
 import productsRouter from './routes/product.route';
 import ordersRouter from './routes/order.route';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
+const DatabaseName = "crm_dev_db";
 const PORT = 3200;
+
+// Connecting app to MongoDB
+mongoose.Promise = global.Promise;
+mongoose.connect(`mongodb://localhost/${DatabaseName}`)
+        .then(() => console.log("Connected to MongoDB!"));
+
+// BodyParser setup
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 // Serve static files
 app.use(express.static("public"));
@@ -17,17 +28,17 @@ app.use(express.static("public"));
 const openapiSpec = yaml.load('./openapi.yaml');
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
-// Attach routers
+// Adding routers
 app.use('/clients', clientsRouter);
 app.use('/products', productsRouter);
 app.use('/orders', ordersRouter);
 
-// Redirect root to swagger
+// Redirecting root to swagger
 app.get("/", (req: Request, res: Response) => {
     res.redirect('/swagger');
 });
 
-// Start server
+// Starting server
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
